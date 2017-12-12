5f86027c121611b938e96b3beb047424bffe7d0c
Gideon <Gideon@DESKTOP-SRGSFBC>
2017-12-12 22:59:17 +0700

    f

--- PMS/Persistence/Repository/CouncilRepository.cs
+++ PMS/Persistence/Repository/CouncilRepository.cs
@@ -35,6 +35,9 @@ namespace PMS.Persistence.Repository
                  .Include(c => c.CouncilEnrollments)
                     .ThenInclude(l => l.CouncilRole)
                 .Include(c => c.Group)
+                    .ThenInclude(p => p.Project)
+                .Include(c => c.Group)
+                    .ThenInclude(p => p.Lecturer)    
                 .SingleOrDefaultAsync(s => s.CouncilId == id);
         }
 
@@ -104,7 +107,7 @@ namespace PMS.Persistence.Repository
                 IsDeleted = false,
                 isMarked = false,
                 Percentage = lecturerInformations.Secretary.ScorePercent,
-                CouncilRole = await context.CouncilRoles.FirstOrDefaultAsync(c => c.CouncilRoleName == "Serectory"),
+                CouncilRole = await context.CouncilRoles.FirstOrDefaultAsync(c => c.CouncilRoleName == "Secretary"),
                 Lecturer = await context.Lecturers.FindAsync(lecturerInformations.Secretary.LecturerId)
             };
 
@@ -125,7 +128,7 @@ namespace PMS.Persistence.Repository
                 isMarked = false,
                 Percentage = lecturerInformations.Supervisor.ScorePercent,
                 CouncilRole = await context.CouncilRoles.FirstOrDefaultAsync(c => c.CouncilRoleName == "Supervisor"),
-                Lecturer = await context.Lecturers.FindAsync(lecturerInformations.Supervisor.LecturerId)
+                Lecturer = council.Group.Lecturer
             };
 
             context.CouncilEnrollments.Add(presidentCouncilEnrollment);
@@ -249,6 +252,11 @@ namespace PMS.Persistence.Repository
                 //poor you bro
                 council.ResultGrade = "F";
             }
+        }
+
+        public CouncilResource FillLecturersInformation(CouncilResource councilResource)
+        {
+            throw new NotImplementedException();
         }
     }
 }
